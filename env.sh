db88c18d5a05ce34b4f77b8b3689cba727158b44
黒ヰ樹 <3456416-tkithrta@users.noreply.gitlab.com>
2022-08-13 20:52:05 +0000

    アップデート .env.example, env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "HOST=127.0.0.1" > .env
+echo "PORT=8080" >> .env
 od -vAn -tx1 -w32 -N32 /dev/urandom | tr -d ' ' >> secret.txt
-echo "SECRET=$(tail -n1 secret.txt)" > .env
+echo "SECRET=$(tail -n1 secret.txt)" >> .env
 ssh-keygen -b 4096 -m PKCS8 -t rsa -N '' -f id_rsa
 echo "PRIVATE_KEY=\"$(cat id_rsa | sed -z 's/\n/\\n/g')\"" >> .env
+
